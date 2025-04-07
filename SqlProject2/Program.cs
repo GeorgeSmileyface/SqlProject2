b460ae01c8fb4e8d44d711a5266680c0ebbf3c11
GeorgeSmileyface <104126452+GeorgeSmileyface@users.noreply.github.com>
2025-04-07 10:54:25 +0200

    Add project files.

---
+++ SqlProject2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System;
+using System.Data.SqlClient;
+
+string connectionString = "Server = localhost; Database = SqlTeachingDb; User Id = sa; Password = 1234; Trusted_Connection = True;";
+
+SqlConnection connection = new SqlConnection(connectionString);
+
+SqlCommand command = connection.CreateCommand();
+string sql = $"INSERT INTO Persons (PersonID, FirstName, LastName, Address, City) VALUES ({1}, 'Peter', 'Buron', 'Here 12', 'Vejle')";
+
+command.CommandText = sql;
+
+connection.Open();
+
+command.ExecuteNonQuery();
+
+Console.ReadKey();
